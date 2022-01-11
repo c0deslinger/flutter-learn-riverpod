23e0e56337c7d25f53319fe1674cb997675831a5
Yusuf Fachroni <yusuf.fachroni@gmail.com>
2022-01-11 20:36:28 +0700

    learn riverpod click counter

---
+++ android/app/src/main/kotlin/com/example/flutter_learn_riverpod/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_learn_riverpod
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class CounterNotifier extends ChangeNotifier {
+  int _value = 0;
+  int get value => _value;
+  void incrementValue() {
+    _value++;
+    notifyListeners();
+  }
+}
+
+final counterProvider = ChangeNotifierProvider((ref) => CounterNotifier());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  void _incrementCounter(BuildContext context) {
+    context.read(counterProvider).incrementValue();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Consumer(builder: (context, watch, child) {
+              final counterNotifier = watch(counterProvider);
+              return Text('${counterNotifier.value}');
+            }),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _incrementCounter(context),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
